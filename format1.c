cb7ed2dfc93d4a6c8e290eb9ee8ff50cff69fa2a
KimuchuJr <kimuchukim@gmail.com>
2023-03-28 05:52:36 -0700

    Conversion specifiers

---
+++ format1.c
@@ -0,0 +1,86 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _printf - print a formatted string
+ *
+ * @format: character string to print composed of 0 or more directives
+ *
+ * Description: write output to standard output stream
+ *
+ * Return: number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	int index, count;
+	va_list data;
+	char c;
+
+	index = count = 0;
+	va_start(data, format);
+	for (; *format && *(format + index); index++)
+	{
+		for (; *(format + index) && *(format + index) != '%'; index++, count++)
+			_putchar(*(format + index));
+		if (!format[index])
+			return (count);
+		else if (*(format + index) == '%')
+		{
+			index++;
+			c = *(format + index);
+			if (c == '%')
+				_putchar('%');
+			else
+				count += process_format(data, c);
+			count++;
+		}
+		if (!format[index])
+			return (count);
+	}
+	va_end(data);
+	return (count);
+}
+
+
+/**
+ * process_format - process a format specifier
+ *
+ * @list: the variable argument list
+ * @ch: character to process
+ *
+ * Return: number of characters printed
+ */
+int process_format(va_list list, char ch)
+{
+	int j = 0;
+	int count = 0;
+	printer_t p[] = {
+		{ 'c', print_char },
+		{ 's', print_string },
+		{ 'S', print_string_all },
+		{ 'r', print_string_rev },
+		{ 'd', print_integer },
+		{ 'i', print_integer },
+		{ 'o', print_octal },
+		{ 'x', print_hex_lower },
+		{ 'X', print_hex_upper },
+		{ 'b', print_binary },
+		{ 'u', print_unsigned },
+		{ 'R', print_rot13 },
+		{ 0, NULL }
+	};
+
+	while (p[j].ch)
+	{
+		if (p[j].ch == ch)
+		{
+			count += p[j].print(list);
+			break;
+		}
+		++j;
+	}
+	return (count);
+}
---
+++ format3.c
@@ -0,0 +1,111 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * print_char - prints a character
+ *
+ * @list: the variable argument list
+ *
+ * Return: (0)
+ */
+int print_char(va_list list)
+{
+	char ch = va_arg(list, int);
+
+	_putchar(ch);
+	return (0);
+}
+
+/**
+ * print_string_all - prints a string to standard output
+ *
+ * @list: the variable argument list
+ *
+ * Description: for non-printable character print \x followed
+ * by the ASCII code value in hexadecimal (uppercase, 2 characters)
+ *
+ * Return: number of characters printed
+ */
+int print_string_all(va_list list)
+{
+	char *result = va_arg(list, char*);
+	int index, length;
+
+	index = length = 0;
+	while (*(result + index))
+	{
+		if ((result[index] > 0 && result[index] < 32) ||
+			result[index] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			if (result[index] <= 15)
+			{
+				_putchar('0');
+				++length;
+			}
+			length += convert((int)result[index], 16, 'X');
+			length += 2;
+		}
+		else
+		{
+			_putchar(*(result + index));
+			++length;
+		}
+		++index;
+	}
+	return (length);
+}
+
+/**
+ * print_string - prints a string to standard output
+ *
+ * @list: the variable argument list
+ *
+ * Return: number of characters printed
+ */
+int print_string(va_list list)
+{
+	char *result = va_arg(list, char*);
+	int length;
+
+	length = 0;
+	while (*(result + length))
+	{
+		_putchar(*(result + length));
+		++length;
+	}
+	return (length - 1);
+}
+
+/**
+ * print_string_rev - print reverse of a string
+ *
+ * @list: variable argument list
+ *
+ * Return: number of characters printed
+ */
+int print_string_rev(va_list list)
+{
+	int i, len;
+	char *str = va_arg(list, char *);
+
+	len = _strlen(str);
+	for (i = len - 1; i >= 0; --i)
+		_putchar(*(str + i));
+	return (len - 1);
+}
+
+/**
+ * _strlen - evaluate the length of a string
+ * @str: the string to be processed
+ * Return: the length of the the string
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+	return (i);
+}
